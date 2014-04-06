846279c221846f0cdae1d2342c757013b12668a0
Peege151 <Pjsulli2k@gmail.com>
2014-04-06 16:51:13 -0400

    First Commit

---
+++ Whopays/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whopays::Application.config.secret_key_base = 'bad3e1675e6b3b22de2b8a142f1c2f0b4e0f7a1bfa1e4a4665eccbb137ba9e6524e44a092769e16617ac3033dc6e5c0730dd905931b60345202a9bd084348ff9'
