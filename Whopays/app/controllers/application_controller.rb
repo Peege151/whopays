e3d5621abbd431fef8ad71beb99fdc7a48c6d621
Edward Yu <anenomaly@gmail.com>
2014-04-07 02:13:12 -0400

    Setup gem requirement for Venmo using the documentations. Added SessionsController + routes + omniauth.rb + setup necessary to use Omniauth in the application_controller.rb

--- Whopays/app/controllers/application_controller.rb
+++ Whopays/app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  use OmniAuth::Strategies::Venmo
+
+
   def home
   end
 end
