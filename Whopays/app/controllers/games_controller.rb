88bf84b64d1418c2adb802876cb0c7cf59fcf6d3
Peege151 <Pjsulli2k@gmail.com>
2014-04-09 13:31:19 -0400

    Add Redirect To Pool Joined, Add 'Join' Functionality From Pool Index

--- Whopays/app/controllers/games_controller.rb
+++ Whopays/app/controllers/games_controller.rb
@@ -1,27 +1,43 @@
 class GamesController < ApplicationController
-	  def new
+    before_action :set_game, only: [:show, :edit, :update, :destroy]
+
+
+    def new
 		@game = Game.new(params[:game])
+		@pool = Pool.where(:id => params[:pool_id])
+
 	  end
 
-	  def create
-		@pool = Pool.find_by(params[:id])
-		@game = current_user.games.build(params[:game])
-	  end
+
 
 	  def create
-    	@game = current_user.games.build(params[:game])
+	  	@pool = Pool.find_by(params[:id])
+    	@game = current_user.games.build(pool_id: params[:pool_id])# :conditions => ["user_id != ?", current_user.id, "pool_id !=?", pool_id]
       if @game.save
         flash[:success] = "Game Created! Good Work!"
-        render 'static_pages/home'
+        redirect_to  @game.pool 
+        
       else
+        flash[:fail] = "Something Happened..."
         render 'static_pages/home'
       end
+    end
+
+    def destroy
+    		  @game.destroy
+    		  respond_to do |format|
+      		format.html { redirect_to pools_url }
+      		format.json { head :no_content }
+    		end
+  	end
   
   	# private
 
        def game_params
-      	params.require(:game)
+      	params.require(:game).permit(:user_id, :pool_id)
        end
-    end
-    
+
+      def set_game
+        @game = Game.find(params[:id])
+      end    
 end
