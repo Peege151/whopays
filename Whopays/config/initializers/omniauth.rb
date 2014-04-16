367cfbd8a4c1b401ab64d8027bbce99871194ec7
Peege151 <Pjsulli2k@gmail.com>
2014-04-16 11:21:21 -0400

    See Venmo Login

--- Whopays/config/initializers/omniauth.rb
+++ Whopays/config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :venmo, '1690', 'JDTMLUjUnWGGBprhKjEDY6LVZufSh5fb', :scope => 'access_feed,access_profile,access_friends,make_payments'
+  provider :venmo, '1691', 'BMHwgUQkcBMTEJSKpbKYyRBg5pktZH7L', :scope => 'access_feed,access_profile,access_friends,make_payments'
 end
