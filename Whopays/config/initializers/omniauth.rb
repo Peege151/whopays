a1753617795a46614876b7078833b4cc8685fc32
Edward Yu <anenomaly@gmail.com>
2014-04-17 00:20:18 -0400

    No modifies done

--- Whopays/config/initializers/omniauth.rb
+++ Whopays/config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :venmo, '1691', 'BMHwgUQkcBMTEJSKpbKYyRBg5pktZH7L',#,{:client_options => {:ssl => {:verify => false}}},
+  provider :venmo, '1691', 'BMHwgUQkcBMTEJSKpbKYyRBg5pktZH7L', {:client_options => {:ssl => {:verify => false}}}
  :scope => 'access_feed,access_profile,access_friends,make_payments'
 end
